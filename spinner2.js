abbddd2b4889b2ddb95cac1cb5a10bb5b1268708
Saim Amir <saimxamir@gmail.com>
2023-03-20 00:09:28 -0400

    add comment for clarification

--- spinner2.js
+++ spinner2.js
@@ -1,22 +1,10 @@
-/*
-Features of spinner:
-- Each character is a delayed write, using setTimeout
-
-- With each delayed write, we start with returning the cursor to the beginning
-
-- We then write the next character in the animation
-
-- Spaces are added after the character so that the cursor moves far enough away from our animation. Removing the space padding at the end of the strings will mean that the cursor will sit on top of the animation, making it harder to see
-*/
-
-
 const rotationArray = ["\r|   ",'\r/   ','\r-   ','\r\\   ',"\r|   ",'\r/   ','\r-   ','\r\\   ',"\r|   ","\n"];
 
 const spinner2 = () =>{
   let delay = 100;
   for (const e of rotationArray) {
     setTimeout(() => {
-      process.stdout.write(e);
+      process.stdout.write(e); //references each index in rotationArray and executes the overwriting order or "animation"
     },delay);
     delay += 200;
   }
